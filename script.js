b774c70ee28a7e88547a6bd873f558222a499032
vsourasrex-design <vsourasrex@gmail.com>
2026-05-05 22:31:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 const scenes = document.querySelectorAll(".scene");
 let index = 0;
-let locked = false;
+let lock = false;
 
 function setScene(i) {
-  if (locked || i === index || i < 0 || i >= scenes.length) return;
+  if (lock || i === index || i < 0 || i >= scenes.length) return;
 
-  locked = true;
-
+  lock = true;
   scenes[index].classList.remove("active");
   index = i;
-
   scenes[index].classList.add("active");
 
-  setTimeout(() => {
-    locked = false;
-  }, 1200);
+  setTimeout(() => lock = false, 1200);
 }
 
 window.addEventListener("wheel", (e) => {
@@ -22,15 +18,12 @@ window.addEventListener("wheel", (e) => {
   else setScene(index - 1);
 });
 
-window.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowDown") setScene(index + 1);
-  if (e.key === "ArrowUp") setScene(index - 1);
-});
 
 
 const imgs = document.querySelectorAll(".gallery-img");
 const lightbox = document.getElementById("lightbox");
 const lightboxImg = document.getElementById("lightboxImg");
+
 const closeBtn = document.getElementById("closeBtn");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
@@ -60,16 +53,46 @@ function prevImg() {
 
 imgs.forEach((img, i) => img.addEventListener("click", () => open(i)));
 
-closeBtn.addEventListener("click", close);
-next.addEventListener("click", nextImg);
-prev.addEventListener("click", prevImg);
+closeBtn.onclick = close;
+next.onclick = nextImg;
+prev.onclick = prevImg;
 
-lightbox.addEventListener("click", (e) => {
+lightbox.onclick = (e) => {
   if (e.target === lightbox) close();
-});
+};
+
+
+
+const videoLightbox = document.getElementById("videoLightbox");
+const openVideo = document.getElementById("openVideo");
+const closeVideo = document.getElementById("closeVideo");
+const modalVideo = document.getElementById("modalVideo");
+
+openVideo.onclick = () => {
+  videoLightbox.classList.add("show");
+  modalVideo.play();
+};
+
+function closeVideoModal() {
+  videoLightbox.classList.remove("show");
+  modalVideo.pause();
+  modalVideo.currentTime = 0;
+}
+
+closeVideo.onclick = closeVideoModal;
+
+videoLightbox.onclick = (e) => {
+  if (e.target === videoLightbox) closeVideoModal();
+};
+
+
 
 document.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") close();
+  if (e.key === "Escape") {
+    close();
+    closeVideoModal();
+  }
+
   if (e.key === "ArrowRight") nextImg();
   if (e.key === "ArrowLeft") prevImg();
 });
